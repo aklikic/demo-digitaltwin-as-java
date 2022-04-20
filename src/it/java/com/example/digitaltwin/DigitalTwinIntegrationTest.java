373411b826d95b0e8062ef13178b8254c3cfa288
Enno Runne <458526+ennru@users.noreply.github.com>
2022-04-20 17:03:26 +0200

    Remove remaining Akka Serverless

--- src/it/java/com/example/digitaltwin/DigitalTwinIntegrationTest.java
+++ src/it/java/com/example/digitaltwin/DigitalTwinIntegrationTest.java
@@ -9,12 +9,12 @@ import org.junit.Test;
 import static java.util.concurrent.TimeUnit.*;
 import static org.junit.Assert.assertEquals;
 
-// This class was initially generated based on the .proto definition by Akka Serverless tooling.
+// This class was initially generated based on the .proto definition by Kalix tooling.
 //
 // As long as this file exists it will not be overwritten: you can maintain it yourself,
 // or delete it so it is regenerated as needed.
 
-// Example of an integration test calling our service via the Akka Serverless proxy
+// Example of an integration test calling our service via the Kalix proxy
 // Run all test classes ending with "IntegrationTest" using `mvn verify -Pit`
 public class DigitalTwinIntegrationTest {
 
